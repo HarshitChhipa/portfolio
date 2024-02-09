e1ddc6d5472ead26fea5c74a9a5308e1cc712138
Harshit Chhipa <harshitkankerwal@gmail.com>
2024-02-09 11:36:34 +0530

    Made repo public as pages is not available in private repo for github free gotta get the pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <h1>
-      Action runner tests auto deployments and made it private. changed a few
-      settings changes.
-    </h1>
-  );
+  return <h1>Action runner tests auto deployments.</h1>;
 }
 
 export default App;
