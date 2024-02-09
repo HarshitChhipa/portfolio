bc63104b026188147a7e65b6fc9d6e35260557ba
Harshit Chhipa <harshitkankerwal@gmail.com>
2024-02-09 11:28:41 +0530

    Testing actions on private repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Action runner tests auto deployments and made it private.</h1>;
+  return (
+    <h1>
+      Action runner tests auto deployments and made it private. changed a few
+      settings changes.
+    </h1>
+  );
 }
 
 export default App;
